77fa864c7e244a608f5fb581f2397dce15725d87
Alexis Ford <118239397+aeford@users.noreply.github.com>
2025-06-09 16:05:40 +0000

    code along video

--- script.js
+++ script.js
@@ -3,17 +3,36 @@ document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
-  // Add movies to the movieList based on the selected genre
+  // Create an empty array to hold the movies for the selected genre
+  let movieList = [];
+
+  // Push movie titles directly to movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    // Add Comedy movies
+    movieList.push("The Mask");
+    movieList.push("Superbad");
+    movieList.push("Home Alone");
+    movieList.push("Dumb and Dumber");
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    // Add Action movies
+    movieList.push("Die Hard");
+    movieList.push("Mad Max: Fury Road");
+    movieList.push("John Wick");
+    movieList.push("The Avengers");
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    // Add Drama movies
+    movieList.push("Forrest Gump");
+    movieList.push("The Shawshank Redemption");
+    movieList.push("The Godfather");
+    movieList.push("A Beautiful Mind");
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    // Add Sci-Fi movies
+    movieList.push("Inception");
+    movieList.push("The Matrix");
+    movieList.push("Interstellar");
+    movieList.push("Star Wars");
   }
 
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${movieList.join(", ")}`;
 });
